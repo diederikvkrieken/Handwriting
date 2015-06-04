4628cd03fd03408c6e0598d1f4aeb741e7dce18e
Bas Gra <s.grasdijk@gmail.com>
2015-06-04 15:48:38 +0200

    multizoning werkend

--- Groep2/features/multiZoning.py
+++ Groep2/features/multiZoning.py
@@ -1,21 +1,20 @@
 image = cv2.imread('n_processed.ppm', 0)
-
-cuts = [(3,1),(1,3),(2,3),(3,2),(3,3),(1,4),(4,1),(4,4),(6,1),(1,6),(6,2),(2,6),(6,6)]
+cuts = [(3, 1), (1, 3), (2, 3), (3, 2), (3, 3), (1, 4), (4, 1), (4, 4), (6, 1), (1, 6), (6, 2), (2, 6), (6, 6)]
 imgwidth, imgheight = image.shape[:2]
 
 feature_vector = []
 
 for n in cuts:
-	x_cuts = n[1]
-	y_cuts = n[2]
-	height = imgheight/y_cuts
-	width = imgwidth/x_cuts
-	imgSize = height*width
+    x_cuts = n[0]
+    y_cuts = n[1]
+    height = int(imgheight / y_cuts)
+    width = int(imgwidth / x_cuts)
+    imgSize = height * width
 
-	for i in range(0,imgheight,height):
-        for j in range(0,imgwidth,width):
-            box = (j, i, j+width, i+height)
-            a = image.crop(box)
-            zeroes <- a.count(0)
-            percentage_black_pixels = (imgSize-zeroes)/imgSize
+    for i in range(0, y_cuts):
+        i = i * height
+        for j in range(0, x_cuts):
+            j = j * width
+            a = image[j:j + width, i:i + height]
+            percentage_black_pixels = a.sum() / imgSize
             feature_vector.append(percentage_black_pixels)
