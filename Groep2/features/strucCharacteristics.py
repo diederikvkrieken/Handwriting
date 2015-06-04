966062988c19d2abebffd736a4262bc9f3cbf192
Bas Gra <s.grasdijk@gmail.com>
2015-06-04 16:49:01 +0200

    structural characteristics werkend

--- Groep2/features/strucCharacteristics.py
+++ Groep2/features/strucCharacteristics.py
@@ -1,23 +1,25 @@
 import cv2
 import numpy as np
-import matplotlib.pyplot as plt
 import math
 from skimage.color import rgb2gray
 from skimage.util import pad
 
+feature_vector = []
 img = cv2.imread('n_processed.ppm', 0)
 
 imgGray = rgb2gray(img)
-imgBW = np.where(img > np.mean(imgGray),1.0,0.0)
-imgBW = pad(imgBW,1, mode='constant', constant_values=1)
+imgBW = np.where(img > np.mean(imgGray), 1.0, 0.0)
+imgBW = pad(imgBW, 1, mode='constant', constant_values=1)
 
 y_hist = imgBW.sum(axis=0)
 x_hist = imgBW.sum(axis=1)
 
-print x_hist
-print y_hist
+feature_vector.append(x_hist)
+feature_vector.append(y_hist)
 
-plt.hist(x_hist, bins=32)
-plt.show()
-plt.hist(y_hist, bins=32)
-plt.show()
+for k in range(0, 72):
+    k = k * 5
+    total = 0
+    for i in range(1, 16):
+        total = total + imgBW[abs(16 - i * math.sin(k)), abs(16 + i * math.cos(k))]
+    feature_vector.append(total)
