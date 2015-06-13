175496cd063ec179ba94adfdc7d6615996ff2520
Bas Gra <s.grasdijk@gmail.com>
2015-06-13 22:16:50 +0200

    Modified Edge Maps aangepast

--- Groep2/features/Digit_features/Modified_Edge_Maps.py
+++ Groep2/features/Digit_features/Modified_Edge_Maps.py
@@ -19,22 +19,24 @@ thin = thinning.thinning(binary)
 
 G = thin
 
-sobelout = Image.new('L', thin.shape[:2])                                       #empty image
+sobelout = Image.new('L', (thin.shape[1],thin.shape[0]))                                       #empty image
 gradx = numpy.array(sobelout, dtype = float)
 grady = numpy.array(sobelout, dtype = float)
+gradtotal = numpy.array(sobelout, dtype = float)
+
 
 sobel_x = [[-1,0,1],
-           [-2,0,2],
+           [-1,0,1],
            [-1,0,1]]
-sobel_y = [[-1,-2,-1],
+sobel_y = [[-1,-1,-1],
            [0,0,0],
-           [1,2,1]]
+           [1,1,1]]
 
 width = thin.shape[1]
 height = thin.shape[0]
 
-for x in range(1, width-1):
-    for y in range(1, height-1):
+for x in range(1, height-1):
+    for y in range(1, width-1):
         px = (sobel_x[0][0] * G[x-1][y-1]) + (sobel_x[0][1] * G[x][y-1]) + \
              (sobel_x[0][2] * G[x+1][y-1]) + (sobel_x[1][0] * G[x-1][y]) + \
              (sobel_x[1][1] * G[x][y]) + (sobel_x[1][2] * G[x+1][y]) + \
@@ -47,26 +49,11 @@ for x in range(1, width-1):
              (sobel_y[2][2] * G[x+1][y+1])
         gradx[x][y] = px
         grady[x][y] = py
-sobeloutmag = scipy.hypot(gradx, grady)
-sobeloutdir = scipy.arctan2(grady, gradx)
 
-for x in range(width):
-    for y in range(height):
-        if (sobeloutdir[x][y]<22.5 and sobeloutdir[x][y]>=0) or \
-           (sobeloutdir[x][y]>=157.5 and sobeloutdir[x][y]<202.5) or \
-           (sobeloutdir[x][y]>=337.5 and sobeloutdir[x][y]<=360):
-            sobeloutdir[x][y]=0
-        elif (sobeloutdir[x][y]>=22.5 and sobeloutdir[x][y]<67.5) or \
-             (sobeloutdir[x][y]>=202.5 and sobeloutdir[x][y]<247.5):
-            sobeloutdir[x][y]=45
-        elif (sobeloutdir[x][y]>=67.5 and sobeloutdir[x][y]<112.5)or \
-             (sobeloutdir[x][y]>=247.5 and sobeloutdir[x][y]<292.5):
-            sobeloutdir[x][y]=90
-        else:
-            sobeloutdir[x][y]=135
+gradtotal = gradx
 
 
 #show the image
-cv2.imshow('image',sobeloutdir)
+cv2.imshow('image',gradtotal)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
