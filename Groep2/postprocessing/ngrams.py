bef2729c254d73acf97a26f71de996f1447c9297
¨Diederik <diederikgreveling@gmail.com>
2015-06-26 18:45:56 +0200

    We can now build digrams or trigrams

--- Groep2/postprocessing/ngrams.py
+++ Groep2/postprocessing/ngrams.py
@@ -10,30 +10,45 @@ class Ngrams():
     def __init__(self):
         pass
 
-    def buildNgram(self, fileName):
+    # This will build an N-Gram given a .dat file For example KNMPDICT.dat. Type will be either 2 or 3 for digram and trigram.
+    def buildNgram(self, fileName, type):
 
         # Read the dictionary
         dictionInput = buildDictionary.DictionaryBuilder().readDict(fileName)
 
         # Reformat dictionary
         reformatedDict = []
-        for word in dictionInput:
-            tempString = ""
-            for char in word[1]:
-                 #print char[0]
-                tempString += (char[:] + " ")
-
-            tempString = tempString[:-1]
-            reformatedDict.append(tempString)
 
         # initiate ngram_vectorizer
-        ngram_vectorizer = CountVectorizer(analyzer= 'word', ngram_range=(2, 2), token_pattern='\\b\\w+\\b')
+        if type == 2:
+            ngram_vectorizer = self.diNgram()
+
+            for word in dictionInput:
+                tempString = "start "
+                for char in word[1]:
+                    #print char[0]
+                    tempString += (char[:] + " ")
+
+                tempString = tempString[:-1]
+                print tempString
+                reformatedDict.append(tempString)
+
+        elif type == 3:
+            ngram_vectorizer = self.triNgram()
+
+            for word in dictionInput:
+                tempString = "start start "
+                for char in word[1]:
+                    #print char[0]
+                    tempString += (char[:] + " ")
+
+                tempString = tempString[:-1]
+                reformatedDict.append(tempString)
+        else:
+            assert False % "Ngram Type must either be 2 or 3"
 
         # Fit.
         counts = ngram_vectorizer.fit_transform(reformatedDict)
-
-        # print ngram_vectorizer.get_feature_names()
-        #print counts.toarray().astype(int)
 
         # Get the differnt "grams"
         matrix_terms = np.array(ngram_vectorizer.get_feature_names())
@@ -45,3 +60,9 @@ class Ngrams():
         dictNgram = dict(zip(matrix_terms, (matrix_freq / float(sum(matrix_freq)))))
 
         return dictNgram
+
+    def diNgram(self):
+        return CountVectorizer(analyzer= 'word', ngram_range=(2, 2), lowercase=False, token_pattern='(?u)[^ \\n]+')
+
+    def triNgram(self):
+        return CountVectorizer(analyzer= 'word', ngram_range=(3, 3), lowercase=False, token_pattern='(?u)[^ \\n]+')
