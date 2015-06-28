2467a6bc5b00780223a57693e28df781cb931a08
¨Diederik <diederikgreveling@gmail.com>
2015-06-28 12:11:30 +0200

    a new postprocessing class

--- Groep2/postprocessing/postprocessing.py
+++ Groep2/postprocessing/postprocessing.py
@@ -1,1 +1,22 @@
 __author__ = 'diederik'
+
+import nGram
+import ngramPredictions
+
+class Postprocessing():
+
+    def __init__(self):
+        pass
+
+    def ngramPostProcessing(self, segmentsOptions, document):
+
+        ngram = nGram.Ngram(document, 3)
+        return ngramPredictions.pathBuilder().run(segmentsOptions, ngram)
+
+    def run(self, segmentsOptions, document = "../KNMPDICT.dat"):
+
+        return self.ngramPostProcessing(segmentsOptions, document)
+
+
+
+
